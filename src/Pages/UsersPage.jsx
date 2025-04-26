371b180a54011519c785c4523d45aeec17c7a5be
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-04-26 18:51:33 +0600

    Delete a user form Ui by EventHandler

--- src/Pages/UsersPage.jsx
+++ src/Pages/UsersPage.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const UsersPage = () => {
-  const users = useLoaderData();
-  console.log(users);
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+
+  const handleDeleteUser = (id)=>{
+      fetch(`http://localhost:5000/users/${id}`,{
+            method: 'DELETE'
+      })
+      .then(res => res.json())
+      .then(data =>{
+           if(data.deletedCount > 0){
+            alert('Deleted Successfully');
+            const remaining = users.filter(user => user._id !== id);
+            setUsers(remaining)
+           }
+      })
+  }
+
   return (
     <div>
       <h3>Users Page.{users.length}</h3>
       <div>
             {
-                  users.map(user => <p key={user._id}>{user.name}  ||email:{user.email}</p>)
+                  users.map(user => <p key={user._id}>{user.name}  ||email:{user.email}|| <button onClick={() =>handleDeleteUser(user._id)}>X</button></p>)
             }
       </div>
     </div>
